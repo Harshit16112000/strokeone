dbce0b73dff9ef2824aa14f247415cd26f12d8a3
harshit <harmim2000@gmail.com>
2020-08-10 12:27:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+
+
+const PORT = process.env.PORT || 5000; 
+// Set Handlebars Middleware
+app.locals.layout = false;
+app.engine('handlebars', exphbs ({
+layoutsDir: __dirname + '/views/layouts',
+}));
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, it is other stuff";
+// Set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff : otherstuff
+    });
+});
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT))
