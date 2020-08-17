daf78f2fa045fdbc5d75eb29d3dac7ce341c08b0
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2020-08-17 15:34:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+// Stock Market app
+
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+// API pk_1ff7017646bb4db3bc790c0dfaaf9352
+
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended : false}));
+
+
+// create call_api function
+function call_api(finishedAPI) {
+     request('https://cloud.iexapis.com/stable/stock/goog/quote?token=pk_1ff7017646bb4db3bc790c0dfaaf9352',{json: true}, (err,res,body) => { 
+if(err){ return console.log(err);}
+
+if(res.statusCode == 200){
+	finishedAPI(body);
+     };
+   });	
+ };
+
+ function call_api(finishedAPI) {
+     request('https://cloud.iexapis.com/stable/stock/goog/quote?token=pk_1ff7017646bb4db3bc790c0dfaaf9352',{json: true}, (err,res,body) => { 
+if(err){ return console.log(err);}
+
+if(res.statusCode == 200){
+	finishedAPI(body);
+     };
+   });	
+ };
+
+
 
 
 
@@ -14,12 +47,28 @@ layoutsDir: __dirname + '/views/layouts',
 app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, it is other stuff";
-// Set handlebars routes
+// Set handlebars index  routes
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff : otherstuff
+	 call_api(function(doneAPI) {
+	 	 res.render('home', {
+    	stock: doneAPI
     });
+	 });  
 });
+
+//call_api(function, req.body.stock_ticker)
+// Set handler
+app.post('/', function (req, res) {
+	 call_api(function(doneAPI) {
+	 	 posted_stuff = req.body.stock_ticker;
+	 	 res.render('home', {
+    	stock: doneAPI,
+    	
+    });
+  },req.body.stock_ticker);
+});
+
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
